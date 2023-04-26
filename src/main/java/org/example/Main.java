d1a58dd5e4e9a352de852f6a713ca47590836c20
Luisanall <127669786+Luisanall@users.noreply.github.com>
2023-04-26 20:04:42 -0300

    version 2.6 me arroja valores desde base de datos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,111 +5,140 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 
-    public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
-        String Participante;
-        //usamos metodo scanner para buscar el nombre del participante
+      public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
+          Scanner Scan = new Scanner ( System.in );
+          System.out.print ( " Introduzca valor del punto" );
+          int puntajePorAcierto = Scan.nextInt ();
+          //declaramos variables que vamos a usar mas adelante
 
-        Scanner Scan = new Scanner ( System.in );
-        System.out.print ( " Introduzca nombre de participante  " );
-        Participante = Scan.nextLine ();
-        //llamamos a la base de Datos
-        DataBase conexion = new DataBase ();
-        //Declaramos variables que vamos a usar mas adelante
-        Connection cn = null;
-        Statement stm = null;
-        ResultSet rs = null;
-        ResultSet rs1 = null;
+          DataBase conexion = new DataBase ();
+          Connection cn = null;
+          Statement stm = null;
+          ResultSet rs = null;
+          int totalpuntos = 0;
+          ArrayList<Partido> partidos;
+          partidos = null;
 
+          try {
+              cn = conexion.conectar ();
+              stm = cn.createStatement ();
 
-        try {
-            try {
-                //crea la conxion con la base de datos
-                cn = conexion.conectar ();
-                stm = cn.createStatement ();
+              partidos = new ArrayList<> ();
 
-                //lee tabla en  base de pronosticos
-                rs1 = stm.executeQuery ( "SELECT COUNT (*) FROM Pronostico  WHERE Participante = " + Participante + " " );
-                while (rs1.next ()) {
+              {
+                  try {
+                      //lee tabla Resultado en base de datos
+                      rs = stm.executeQuery ( "SELECT* FROM Resultado" );
+                      while (rs.next ()) {
+                          //identificación de posicion de columnas
+                          int Ronda = rs.getInt ( 1 );
+                          int id = rs.getInt ( 2 );
+                          String Equipo1 = rs.getString ( 3 );
+                          int CantidadGoles1 = rs.getInt ( 4 );
+                          int CantidadGoles2 = rs.getInt ( 5 );
+                          String Equipo2 = rs.getString ( 6 );
 
-                    //identificacion de columnas en pronostico
-                    Participante = rs1.getString ( 1 );
-                    int id = rs1.getInt ( 2 );
-                    String Equipo1 = rs1.getString ( 3 );
-                    int gana1 = rs1.getInt ( 4 );
-                    int empate = rs1.getInt ( 5 );
-                    int gana2 = rs1.getInt ( 6 );
-                    String Equipo2 = rs1.getString ( 7 );
-                    int totalpuntos = 0;
+                          Partido partido = new Partido ( Ronda, id, Equipo1, CantidadGoles1, CantidadGoles2, Equipo2 );
+                          partidos.add ( partido );
+                          //lee tabla en  base de pronosticos
 
-                    //lee tabla Resultado en base de datos
-                    rs = stm.executeQuery ( "SELECT * FROM Resultado WHERE Equipo1 = " + Equipo1 + "AND Equipo2 = " + Equipo2 + " " );
-                    while (rs.next ()) {
+                          rs = stm.executeQuery ( "SELECT * FROM Pronostico " );
+                          while (rs.next ()) {
+                              //identificacion de columnas en pronostico
+                             String Participante = rs.getString ( 1 );
+                              id = rs.getInt ( 2 );
+                              Equipo1 = rs.getString ( 3 );
+                              int gana1 = rs.getInt ( 4 );
+                              int empate = rs.getInt ( 5 );
+                              int gana2 = rs.getInt ( 6 );
+                              Equipo2 = rs.getString ( 7 );
+                              System.out.println (Participante);
+                              //comparacion de resultados para clasificarlos como ganador, perdedor o empatadosº
+                              // al comparar se le asigna punto por aciertos
+                              if (gana1 == 1 && CantidadGoles1 > CantidadGoles2) {
+                                  Equipo1 = "acertado";
+                                  totalpuntos += puntajePorAcierto;
 
-                        //identificación de posicion de columnas
-                        int Ronda = rs.getInt ( 1 );
-                        id = rs.getInt ( 2 );
-                        Equipo1 = rs.getString ( 3 );
-                        int CantidadGoles1 = rs.getInt ( 4 );
-                        int CantidadGoles2 = rs.getInt ( 5 );
-                        Equipo2 = rs.getString ( 6 );
+                              } else if (gana2 == 1 && CantidadGoles1 < CantidadGoles2) {
+                                  Equipo1 = "acertado";
+                                  totalpuntos += puntajePorAcierto;
 
+                              } else if (empate == 1 && CantidadGoles1 == CantidadGoles2) {
+                                  Equipo1 = "empate";
+                                  Equipo2 = "empate";
+                                  totalpuntos += puntajePorAcierto;
+                              }
+                          }
+                      }
+                  } catch (SQLException ex) {
+                      throw new RuntimeException ( ex );
+                  }
 
-                        //comparacion de resultados para clasificarlos como ganador, perdedor o empatados
+              }
+              Ronda ronda = new Ronda ( 1, partidos.toArray (), 50, 1 );
 
-                        if (gana1 == 1 && CantidadGoles1 > CantidadGoles2) {
-
-                            Equipo1 = "ganador";
-                            totalpuntos++;
-
-                        } else if (gana2 == 1 && CantidadGoles1 < CantidadGoles2) {
-                            Equipo1 = "perdedor";
-                            totalpuntos++;
-
-                        } else if (empate == 1 && CantidadGoles1 == CantidadGoles2) {
-                            Equipo1 = "empate";
-                            Equipo2 = "empate";
-                            totalpuntos++;
-                        }
-                        System.out.print ( "total de puntos es:  " );
-                        System.out.println ( "El participante  " + Participante+ "obtuvo  " + totalpuntos);
-
-                    }
-                }
-
-
-
-            } catch (SQLException e) {
-                System.out.println ( "ocurrió un error en conex a Base de Datos" );
+              List<Pronostico> pronosticos = new ArrayList<> ();
 
 
-            } finally {
-                try {
-                    if (rs != null && rs1 != null) {
-                        rs.close ();
-                        rs1.close ();
-                    }
-                    if (stm != null) {
-                        stm.close ();
-                    }
-                    if (cn != null) {
-                        cn.close ();
-                    }
-                } catch (SQLException e) {
-                    System.out.println ( "Ocurrió un error al cerrar base de Datos " );
-                }
+              for (Partido partido : partidos) {
+                  System.out.println ( "id " + ronda.getId () + "   " + partido.getId () + "   " + partido.getEquipo1 () + "  " + partido.getCantidadGoles1 () + "  " + partido.getEquipo2 () + "  " + partido.getCantidadGoles2 () );
+              }
+              for (Pronostico pronostico : pronosticos) {
+                  System.out.println ( "id " + pronostico.getParticipante () + "  " + pronostico.getPartido ().getId () + "  " + pronostico.getPartido ().getEquipo2 () + " " + pronostico.getResultados () + " " + pronostico.getPartido ().resultados ( pronostico.getEquipo () ) + "\n" );
+
+                  totalpuntos = totalpuntos + pronostico.puntos ();
+              }
+              System.out.println ( "el total es :  " + ronda.getPuntos () * totalpuntos );
 
 
-            }
-        } finally {
+          } catch (SQLException e) {
+              System.out.println ( "ocurrió un error en conex a Base de Datos" );
 
-        }
-    }
+              Ronda ronda = new Ronda ( 1, partidos.toArray (), 50, 1 );
 
 
-    }
+              List<Pronostico> pronostico = new ArrayList<> ();
+
+
+              for (Partido partido : partidos) {
+                  System.out.println ( "id " + ronda.getId () + "   " + partido.getId () + "   " + partido.getEquipo1 () + "  " + partido.getCantidadGoles1 () + "  " + partido.getEquipo2 () + "  " + partido.getCantidadGoles2 () );
+              }
+              for (Pronostico prono : pronostico) {
+                  System.out.println ( "id " + prono.getParticipante () + "  " + prono.getPartido ().getId () + "  " + prono.getPartido ().getEquipo2 () + " " + prono.getResultados () + " " + prono.getPartido ().resultados ( prono.getEquipo () ) + "\n" );
+
+                  totalpuntos = totalpuntos + prono.puntos ();
+              }
+              System.out.print ( "El participante  obtuvo " + totalpuntos + " puntos por este partido. " );
+
+              System.out.println ( "El puntaje por acierto fue de " + puntajePorAcierto );
+
+
+          } finally {
+
+
+                  if (cn != null) {
+                      try {
+                          cn.close ();
+                          if (rs != null) {
+                              rs.close ();
+                              Scan.close ();
+                              System.out.print ( "Hubo error al cerrar base de Datos" );
+                          }
+
+                      } catch (SQLException e) {
+                          throw new RuntimeException ( e );
+                      }
+                  }
+              }
+
+
+          }
+      }
 
